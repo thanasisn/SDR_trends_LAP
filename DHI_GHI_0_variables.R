7cb9005ad4d07a04fd1ba6d8ac70353d35168833
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-10-11 07:17:01 +0300

    Commit 2022-10-11 07:17

--- DHI_GHI_0_variables.R
+++ DHI_GHI_0_variables.R
@@ -2,29 +2,28 @@
 ## Variables
 
 
-CLEARdir                 <- "~/DATA/Broad_Band/CS_id"
-tag                      <- paste0("Natsis Athanasios LAP AUTH ", strftime(Sys.time(), format = "%b %Y" ))
-CS_file                  <- "~/DATA/Common_application/Clear_Sky.Rds"
-common_data              <- "~/MANUSCRIPTS/2022_sdr_trends/data/common_data.Rds"
+CLEARdir       <- "~/DATA/Broad_Band/CS_id"
+tag            <- paste0("Natsis Athanasios LAP AUTH ", strftime(Sys.time(), format = "%b %Y" ))
+CS_file        <- "~/DATA/Common_application/Clear_Sky.Rds"
+common_data    <- "~/MANUSCRIPTS/2022_sdr_trends/data/common_data.Rds"
 
-col_DIR_att              <- "#2166ac"
-col_HOR_att              <- "#4244ac"
-col_DIR_transp           <- "#9970ab"
-col_GLB_att              <- "#1a9850"
+col_DIR_att    <- "#2166ac"
+col_HOR_att    <- "#4244ac"
+col_DIR_transp <- "#9970ab"
+col_GLB_att    <- "#1a9850"
 
 ## https://www.rapidtables.com/calc/time/days-in-year.html
 # Days_of_year             <- 365.25   ## Mean Julian year
 Days_of_year             <- 365.2425 ## Mean Gregorian calendar year
+pch_am         <-  1
+pch_pm         <-  4
+pch_ampm       <- 13 ## try 10
+pch_daily      <- 19
 
-pch_am                   <-  1
-pch_pm                   <-  4
-pch_ampm                 <- 13 ## try 10
-pch_daily                <- 19
-
-MIN_ELEVA                <-  5  ## use data when sun above that
-SZA_BIN                  <-  1
-MIN_N                    <-  4
-SEAS_MIN_N               <-  3
+MIN_ELEVA      <-  5  ## use data when sun above that
+SZA_BIN        <-  1
+MIN_N          <-  4
+SEAS_MIN_N     <-  3
 
 Daily_aggregation_N_lim  <- 50         ## was SUM_THRES
 Daily_confidence_limit   <-  0.99
