e51cef7a7b12c542f641381462e34d396935665f
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-04-26 09:17:02 +0300

    Commit 2023-04-26 09:17

--- DHI_GHI_0_variables.R
+++ DHI_GHI_0_variables.R
@@ -40,10 +40,10 @@ tag                      <- paste0("Natsis Athanasios LAP AUTH ", strftime(Sys.t
 CS_file_13               <- "~/DATA/Common_application/Clear_Sky_13.Rds"
 CS_file_14               <- "~/DATA/Common_application/Clear_Sky_14.Rds"
 CS_file_14               <- "~/DATA/Common_application/Clear_Sky_14.Rds"
-CS_file_14_1             <- "~/DATA/Common_application/Clear_Sky_14_1.Rds"
+CS_file_14_2             <- "~/DATA/Common_application/Clear_Sky_14_2.Rds"
 common_data_13           <- "~/MANUSCRIPTS/2022_sdr_trends/data/common_data_13.Rda"
 common_data_14           <- "~/MANUSCRIPTS/2022_sdr_trends/data/common_data_14.Rda"
-common_data_14_1         <- "~/MANUSCRIPTS/2022_sdr_trends/data/common_data_14_1.Rda"
+common_data_14_2         <- "~/MANUSCRIPTS/2022_sdr_trends/data/common_data_14_2.Rda"
 variables_fl             <- "~/MANUSCRIPTS/2022_sdr_trends/DHI_GHI_0_variables.R"
 data_procsess_fl         <- "~/MANUSCRIPTS/2022_sdr_trends/DHI_GHI_0_data_input.R"
 
