9045abb8185f4b174b57401cb93cb4d83baab774
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-06-27 08:17:02 +0300

    Commit 2023-06-27 08:17

--- gather_citations.R
+++ gather_citations.R
@@ -17,26 +17,32 @@ lines   <- paste(readLines(sourcefile), collapse = "")
 entries <- unique(str_match_all(lines, "@([a-zA-Z0-9]+)[,\\. \\?\\!\\]\\;]")[[1]][, 2])
 entries <- grep("gmail|auth", entries, value = TRUE, invert = TRUE)
 
+
 ## get refs
 bib <- bib2df(mainreposit)
-bib$ABSTRACT <- NA
 
 ## export sub file
 output <- bib[bib$BIBTEXKEY %in% entries, ]
+filesp <- output$FILE
+output$ABSTRACT         <- NA
+output$OWNER            <- NA
+output$GROUPS           <- NA
+output$MODIFICATIONDATE <- NA
+output$TIMESTAMP        <- NA
+output$FILE             <- NA
 df2bib(output, localreposit)
 
+
+
+# link files
 filesp <- output$FILE
 names(output)
 basepath <- dirname(Sys.readlink(mainreposit))
 
 for (af in strsplit(filesp, ":")) {
     cat(length(af),"\n")
-    if (length(af)>=2) {
-        af[2]
-
+    if (length(af) >= 2) {
         sourcefl <- paste0(basepath,"/", af[2])
-
-
         if (file.exists(sourcefl)) {
             dir.create(folderlink)
             file.symlink(sourcefl, paste0(folderlink, "/", basename(af[2])))
