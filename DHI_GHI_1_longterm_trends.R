edc7d5fb91d585e708c5179516fb842df377eacf
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-12-06 12:39:26 +0200

    Commit 2023-12-06 12:39

--- DHI_GHI_1_longterm_trends.R
+++ DHI_GHI_1_longterm_trends.R
@@ -470,12 +470,17 @@ for (DBn in dbs) {
 
         par("mar" = c(3, 4, 2, 1))
 
+        if (grepl("wattGLB", yvar)) {
+            acol <- "green"
+        } else {
+            acol <- get(paste0(c("col", unlist(strsplit(yvar, split = "_"))[1:2]),
+                              collapse = "_"))
+        }
+
         ## plot data
         plot(dataset$doy, dataset$V2,
-             pch  = ".",
-             col  = get(paste0(c("col",
-                                 unlist(strsplit(avar, split = "_"))[1:2]),
-                               collapse = "_")),
+             pch      = ".",
+             col      = acol,
              cex      = 2,
              main     = paste("Climatology", translate(DBn), translate(avar)),
              cex.main = 0.8,
